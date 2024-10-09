c510e466711d7e1436c2e01f325288db59d04daf
HilaScharf <27hscharf@gannacademy.org>
2024-10-09 09:56:10 -0400

    the nearly finished product

--- src/App.java
+++ src/App.java
@@ -4,13 +4,14 @@ public class App extends PApplet {
     // arrow keys are not pressed
     boolean moveLeft = false;
     boolean moveRight = false;
-    int speed = 3;
+    double speed = 30;
+
     public static void main(String[] args) {
         PApplet.main("App");
     }
 
     // Flag to track if a collision is detected
-    boolean collisionDetected = false;
+    int collisionDetected = 0;
 
     // the character starts in the middle of the screen
     float characterX = 350;
@@ -34,7 +35,7 @@ public class App extends PApplet {
     float pothole600X;
     float pothole6Y = -975;
     // point counter variable
-    int points = 0;
+    double points = 0;
 
     public void setup() {
         // background is green
@@ -56,7 +57,10 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        if (!collisionDetected) {
+        if (collisionDetected == 0) {
+            startScreen();
+        }
+        if (collisionDetected == 1) {
             // refresh background
             background(26, 189, 8);
             // potholes
@@ -71,27 +75,44 @@ public class App extends PApplet {
             ellipse(characterX, 500, 50, 50);
             // move the character
             if (moveLeft == true) {
-                characterX -= 3;
+                characterX -= speed/10 - 0.5;
             }
             if (moveRight == true) {
-                characterX += 3;
+                characterX += speed/10 - 0.5;
             }
             checkingCollision();
-
-        } else {
+            fill(255);
+            textSize(30);
+            text("Score: " + points, 25, 20);
+            text("Speed: " + speed/10, 25, 50);
+            changeSpeed();
+            
+        } else if(collisionDetected == 2) {
             endScreen();
             playAgain();
         }
     }
 
     public void playAgain() {
-        if (mousePressed) {
-            if (mouseX >= 250 && mouseX <= 450 &&
+        if (mousePressed && mouseX >= 250 && mouseX <= 450 &&
                     mouseY >= 250 && mouseY <= 350) {
-                collisionDetected = false;
+                collisionDetected = 1;
                 reset();
-            }
+            
         }
+    }
+
+    public void startScreen() {
+        background(5);
+        strokeWeight(5);
+        stroke(3, 252, 32);
+        fill(5);
+        rect(250, 250, 200, 100);
+        fill(3, 252, 32);
+        textSize(30);
+        text("Start", 290, 310);
+        playAgain();
+        text("you are running to your house through the forest. Dont run into")
     }
 
     // Function to detect collision between two circles
@@ -102,137 +123,123 @@ public class App extends PApplet {
 
     public void endScreen() {
         background(5);
-        fill(255, 0, 0); // Red
-        textSize(50);
-        textAlign(CENTER, CENTER);
-        text("You lose!", width / 2, height / 5);
+        winOrLose();
         fill(255);
         textSize(30);
-        text("Score: " + points, width / 7, height / 10);
-        text("Your speed was " + speed, width / 5, height / 7);
-        
+        text("Score: " + points, 25, 20);
+        text("Your speed was " + speed/10, 25, 50);
+
         strokeWeight(5);
         stroke(3, 252, 32);
         fill(5);
         rect(250, 250, 200, 100);
         fill(3, 252, 32);
-        text("Play again", 350, 290);
+        text("Play again", 290, 310);
     }
-
+    public void winOrLose() {
+        //if()
+        fill(255, 0, 0); // Red
+        textSize(50);
+        text("You lose!", 260, 200);
+    }
     public void checkingCollision() {
         if (detectCollision(pothole1X, pothole1Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole2X, pothole2Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole3X, pothole3Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole4X, pothole4Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole100X, pothole1Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole200X, pothole2Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole300X, pothole3Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
         if (detectCollision(pothole400X, pothole4Y, 25, characterX, 500, 15)) {
-            collisionDetected = true; // Set the flag to true when collision occurs
+            collisionDetected = 2; // Set the flag to true when collision occurs
         }
     }
 
     public void potHoleMove() {
         if (pothole1Y < 850) {
-            pothole1Y += speed;
+            pothole1Y += speed/10;
             if (pothole1Y >= 850) {
                 pothole1Y = -50;
                 pothole1X = random(0, 360);
-                points++;
-                speed += 0.5;
             }
         }
         // pothole 2
         ellipse(pothole2X, pothole2Y, 70, 70);
         if (pothole2Y < 850) {
-            pothole2Y += speed;
+            pothole2Y += speed/10;
             if (pothole2Y >= 850) {
                 pothole2Y = -50;
                 pothole2X = random(0, 360);
-                points++;
-                speed += 0.5;
             }
         }
         // pothole 3
         ellipse(pothole3X, pothole3Y, 70, 70);
         if (pothole3Y < 850) {
-            pothole3Y += speed;
+            pothole3Y += speed/10;
             if (pothole3Y >= 850) {
                 pothole3Y = -50;
                 pothole3X = random(0, 360);
-                points++;
-                speed += 0.5;
             }
         }
         // pothole 4
         ellipse(pothole4X, pothole4Y, 70, 70);
         if (pothole4Y < 850) {
-            pothole4Y += speed;
+            pothole4Y += speed/10;
             if (pothole4Y >= 850) {
                 pothole4Y = -50;
                 pothole4X = random(0, 360);
-                points++;
-                speed += 0.5;
             }
         }
         // pothole 1.2
         ellipse(pothole100X, pothole1Y, 70, 70);
         if (pothole1Y < 850) {
-            pothole1Y += speed;
+            pothole1Y += speed/10;
             if (pothole1Y >= 850) {
                 pothole1Y = -50;
                 pothole100X = random(425, 700);
-                points++;
-                speed += 0.5;
             }
         }
 
         // pothole 2.2
         ellipse(pothole200X, pothole2Y, 70, 70);
         if (pothole2Y < 850) {
-            pothole2Y += speed;
+            pothole2Y += speed/10;
             if (pothole2Y >= 850) {
                 pothole2Y = -50;
                 pothole200X = random(425, 700);
-                points++;
-                speed += 0.5;
             }
         }
         // pothole 3.2
         ellipse(pothole300X, pothole3Y, 70, 70);
         if (pothole3Y < 850) {
-            pothole3Y += speed;
+            pothole3Y += speed/10;
             if (pothole3Y >= 850) {
                 pothole3Y = -50;
                 pothole300X = random(425, 700);
-                points++;
-                speed += 0.5;
             }
         }
 
         // pothole 4.2
         ellipse(pothole400X, pothole4Y, 70, 70);
         if (pothole4Y < 850) {
-            pothole4Y += speed;
+            pothole4Y += speed/10;
             if (pothole4Y >= 850) {
                 pothole4Y = -50;
                 pothole400X = random(425, 700);
-                points++;
-                speed += 0.5;
             }
         }
     }
@@ -251,18 +258,26 @@ public class App extends PApplet {
         moveLeft = false;
         moveRight = false;
     }
+
     public void reset() {
         // the character starts in the middle of the screen
-     characterX = 350;
-    // potholes start y
-     pothole1Y = -50;
-     pothole2Y = -225;
-     pothole3Y = -400;
-     pothole4Y = -575;
-     pothole5Y = -750;
-     pothole6Y = -975;
-    // point counter variable
-     points = 0;
-     speed = 3;
+        characterX = 350;
+        // potholes start y
+        pothole1Y = -50;
+        pothole2Y = -225;
+        pothole3Y = -400;
+        pothole4Y = -575;
+        pothole5Y = -750;
+        pothole6Y = -975;
+        // point counter variable
+        points = 0;
+        speed = 30;
     }
+   
+   public void changeSpeed() {
+    if(frameCount % 300 == 0) {
+        speed++;
+        points ++;
+    }
+   }
 }
